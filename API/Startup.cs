ae1d5811936653ce99e69ec32f22d3ae95c84d19
Betemariam Tegie <bete241011@gmail.com>
2021-08-17 17:04:37 +0300

    Adding a not found endpoint error handler.

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
