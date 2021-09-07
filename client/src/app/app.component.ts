b5f3436d28b8d075e3fa111e505095cd4cb82cf7
Betemariam Tegie <bete241011@gmail.com>
2021-09-07 14:17:57 +0300

    Using the replay subject.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,10 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService, private accountService: AccountService) {}
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.loadBasket();
@@ -19,16 +22,13 @@ export class AppComponent implements OnInit {
 
   loadCurrentUser() {
     const token = localStorage.getItem('token');
-    if (token) {
-      this.accountService.loadCurrentUser(token)
-        .subscribe(() => {
-          console.log('loadded user');
-        },
-        error => {
-          console.log(error);
-        });
-
-    }
+    this.accountService.loadCurrentUser(token).subscribe(
+      () => {
+        console.log('loadded user');
+      },
+      error => {
+        console.log(error);
+      });
   }
 
   loadBasket() {
