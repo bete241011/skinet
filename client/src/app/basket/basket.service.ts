60a34514465e077f9c4c4ccd1fa21ea9cfa8555a
Betemariam Tegie <bete241011@gmail.com>
2021-09-17 13:34:34 +0300

    Adding the payment intent function.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,10 +20,21 @@ export class BasketService {
 
   constructor(private http: HttpClient) {}
 
+  createPaymentIntent() {
+    return this.http.post<IBasket>(this.baseUrl + 'payments/' + this.getCurrentBasketValue()?.id, {})
+      .pipe(
+        map((basket: IBasket) => {
+          this.basketSource.next(basket);
+          console.log(this.getCurrentBasketValue());
+        })
+      )
+  }
+
   setShippingPrice(deliveryMethod: IDeliveryMethod) {
     this.shipping = deliveryMethod.price;
     const basket = this.getCurrentBasketValue();
     basket!.deliveryMethodId = deliveryMethod.id;
+    // basket!.shippingPrice = deliveryMethod.price;
     this.calculateTotals();
     this.setBasket(basket);
   }
@@ -32,6 +43,7 @@ export class BasketService {
     return this.http.get<IBasket>(this.baseUrl + 'basket?id=' + id).pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
+        // this.shipping = basket.shippingPrice!;
         this.calculateTotals();
       })
     );
